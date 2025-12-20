8e528e92e4502fa1894309c80673308fbec5bf40
KT <aprgxy@gmail.com>
2025-12-21 08:05:04 +0900

    7. 【演習】クラス図をTypeScriptで実装 - practice1,2.ts を作成

--- 02-uml/classDiagramPractice/practice2.ts
+++ 02-uml/classDiagramPractice/practice2.ts
@@ -1,3 +1,23 @@
-export {};
-
 // クラス図 演習2
+class Client{
+    private shape: Shape;
+}
+
+interface Shape{
+    calcArea(): number;
+}
+
+class Rectangle implements Shape{
+    private width: number;
+    private height: number;
+    calcArea(): number {
+        return this.width * this.height;
+    }
+}
+
+class Circle implements Shape{
+    private radius: number;
+    calcArea(): number {
+        return this.radius * this.radius * Math.PI;
+    }
+}
