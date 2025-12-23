bd64227da2abf6ec3a382b6590c20188508aa64c
KT <aprgxy@gmail.com>
2025-12-24 07:08:22 +0900

    23. インターフェース分離の原則【コーディング Part2】

--- 03-solid-principle/interfaceSegregation/goodExample.ts
+++ 03-solid-principle/interfaceSegregation/goodExample.ts
@@ -1,1 +1,50 @@
-export {};
+  interface Flyable {
+    fly();
+  }
+
+  interface Movable {
+    start();
+    stop();
+  }
+
+  interface Vehicle {
+    name: string;
+    color: string;
+  }
+
+  class Airplane implements Flyable, Movable, Vehicle {
+    constructor(public name: string, public color: string) {}
+
+    start() {
+      console.log("start!");
+    }
+    stop() {
+      console.log("stop!");
+    }
+    fly() {
+      console.log("fly!");
+    }
+  }
+
+  class Car implements Movable, Vehicle {
+    constructor(public name: string, public color: string) {}
+    start() {
+      console.log("start!");
+    }
+    stop() {
+      console.log("stop!");
+    }
+    fly() {
+      throw new Error("車は空を飛べません");
+    }
+  }
+
+  function run() {
+    const v1 = new Airplane("AirBus", "white");
+    const v2 = new Car("Prius", "black");
+
+    v1.fly();
+    v2.start();
+  }
+
+  run();
