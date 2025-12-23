2728c097851bfafea7bfdc1ebab8aae0ce392c2d
KT <aprgxy@gmail.com>
2025-12-24 07:08:34 +0900

    26. 依存性逆転の原則

--- 03-solid-principle/dependencyInversion/goodExample.ts
+++ 03-solid-principle/dependencyInversion/goodExample.ts
@@ -1,1 +1,77 @@
-export {};
+namespace dependencyInversionGood {
+  class User {}
+
+  class UserController {
+    private userService: IUserService;
+
+    constructor(userService: IUserService) {
+      this.userService = userService;
+    }
+
+    create(user: User): User {
+      return this.userService.create(user);
+    }
+
+    findById(id: string): User {
+      return this.userService.findById(id);
+    }
+  }
+
+  interface IUserService {
+    create(user: User): User;
+    findById(id: string): User;
+  }
+
+  class UserService implements IUserService {
+    constructor(private userRepository: IUserRepository) {
+      this.userRepository = userRepository;
+    }
+
+    create(user: User): User {
+      return this.userRepository.create(user);
+    }
+
+    findById(id: string): User {
+      return this.userRepository.findById(id);
+    }
+  }
+
+  interface IUserRepository {
+    create(user: User): User;
+    findById(id: string): User;
+  }
+
+  class UserRdbRepository implements IUserRepository {
+    create(user: User): User {
+      console.log("RDBにUserを登録");
+      return user;
+    }
+
+    findById(id: string): User {
+      console.log(`ID: ${id}のユーザーを検索`);
+      return new User();
+    }
+  }
+
+  class TestRepository implements IUserRepository {
+    create(user: User): User {
+      console.log("RDBにUserを登録");
+      return user;
+    }
+
+    findById(id: string): User {
+      console.log(`Test ID: ${id}のユーザーを検索`);
+      return new User();
+    }
+  }
+
+  function run() {
+    // const repository = new UserRdbRepository;
+    const repository = new TestRepository;
+    const service = new UserService(repository);
+    const userController = new UserController(service);
+    userController.findById("123");
+  }
+
+  run();
+}
