e76eb4fd9080f10ade03c352f7e4958bc3e79c3a
KT <aprgxy@gmail.com>
2026-01-08 08:09:34 +0900

    41. Facade【コーディング】

--- 04-design-pattern-part1/facade/facade.ts
+++ 04-design-pattern-part1/facade/facade.ts
@@ -1,2 +1,44 @@
 export {}
 
+class Product {
+    getProduct(name: string) {
+        console.log(`${name}を取得しました`);
+    }
+}
+
+class Payment {
+    makePayment(name: string) {
+        console.log(`${name}の支払いを行いました`);
+    }
+}
+
+class Invoice {
+    sendInvoice(name: string) {
+        console.log(`${name}の請求書を送信しました`);
+    }
+}
+
+class Order {
+    placeOrder(name: string) {
+        console.log("注文開始");
+
+        const product = new Product();
+        product.getProduct(name);
+
+        const payment = new Payment();
+        payment.makePayment(name);
+
+        const invoice = new Invoice();
+        invoice.sendInvoice(name);
+
+        console.log("注文は正常に完了しました");
+    }
+}
+
+function run() {
+    const name = "デザインパターン本";
+    const order = new Order();
+    order.placeOrder(name);
+}
+
+run();
